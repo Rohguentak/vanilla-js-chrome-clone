343e7446d9fb6ffc63bf7c86bcb43a6ada38fbf9
Rohguentak <rhgt78@gmail.com>
2021-01-21 15:53:59 +0900

    modify css greeting part

--- greeting.js
+++ greeting.js
@@ -1,14 +1,17 @@
+const hello = document.querySelector(".say-hello");
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greeting");
 const USER_LS = "USER";
 const SHOWING_CN = "showing";
+const JS_FORM = "js-form";
+const todoFormForShow = document.querySelector(".js-todoForm");
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit() {
+function handleSubmit(event) {
   event.preventDefault();
   const value = input.value;
   paintGreeting(value);
@@ -16,12 +19,16 @@ function handleSubmit() {
 }
 
 function askForName() {
+  hello.classList.add(SHOWING_CN);
   form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
+  form.classList.remove(JS_FORM);
+  hello.classList.remove(SHOWING_CN);
+  todoFormForShow.classList.add(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }

5a4a124eeca11bc3c0e4c2c2f1e286e7c7ba05a7
Rohguentak <rhgt78@gmail.com>
2021-01-21 16:24:03 +0900

    divide form.css and modify greeting.js to apply

--- greeting.js
+++ greeting.js
@@ -1,11 +1,12 @@
 const hello = document.querySelector(".say-hello");
+const askTodo = document.querySelector(".ask-todo-list");
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greeting");
+const todoFormForShow = document.querySelector(".js-todoForm");
 const USER_LS = "USER";
 const SHOWING_CN = "showing";
-const JS_FORM = "js-form";
-const todoFormForShow = document.querySelector(".js-todoForm");
+const FORM_DESIGN = "form-design";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
@@ -20,17 +21,16 @@ function handleSubmit(event) {
 
 function askForName() {
   hello.classList.add(SHOWING_CN);
-  form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  form.classList.remove(JS_FORM);
+  form.classList.remove(FORM_DESIGN);
   hello.classList.remove(SHOWING_CN);
-  todoFormForShow.classList.add(SHOWING_CN);
+  askTodo.classList.add(SHOWING_CN);
+  todoFormForShow.classList.add(FORM_DESIGN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+  greeting.innerText = `Hello ${text}!`;
 }
 
 function loadName() {

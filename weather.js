d74d5989dd69fb68993517984b7036fe31d083e5
Rohguentak <rhgt78@gmail.com>
2021-01-26 17:03:40 +0900

    add weather info in screen

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 const COORDS = "coordsinfo";
 const API_KEY = "9753ebb1bec25f2f42c01f9c94fe6c2e";
-const geo_info = document.querySelector(".geo-info");
+const wheather_info = document.querySelector(".wheather-info");
 
 function getWeather(lat, long) {
   fetch(
@@ -10,16 +10,45 @@ function getWeather(lat, long) {
       return wheatherdata.json();
     })
     .then(function (data) {
+      paintWeather(data);
       console.log(data);
     });
+}
+
+function paintWeather(data) {
+  const imageForIconCelsius = new Image();
+  const imageForIconCelsius2 = new Image();
+  imageForIconCelsius.src = `pictures/celsius-degrees-symbol-of-temperature.png`;
+  imageForIconCelsius.classList.add("cel_icon");
+  imageForIconCelsius2.src = `pictures/celsius-degrees-symbol-of-temperature.png`;
+  imageForIconCelsius2.classList.add("cel_icon");
+  const location = data.name;
+  const tempNow = data.main.temp;
+  const tempFeelLike = data.main.feels_like;
+  const humidity = data.main.humidity;
+  const spanForTempNow = document.createElement("span");
+  const spanForTempFeelLike = document.createElement("span");
+  spanForTempNow.classList.add("temp");
+  spanForTempFeelLike.classList.add("temp_feel");
+  const spanForHumidity = document.createElement("span");
+  spanForTempNow.innerText = `Temperature: ${tempNow}`;
+  spanForTempFeelLike.innerText = `Feels_like: ${tempFeelLike}`;
+  spanForHumidity.innerText = `Humidity: ${humidity}%`;
+  wheather_info.appendChild(spanForTempNow);
+  wheather_info.appendChild(spanForTempFeelLike);
+  wheather_info.appendChild(spanForHumidity);
+  const temp = wheather_info.querySelector(".temp");
+  const temp_feel = wheather_info.querySelector(".temp_feel");
+  temp.appendChild(imageForIconCelsius);
+  temp_feel.appendChild(imageForIconCelsius2);
 }
 function paintCoords(parsedCoords) {
   const spanForLat = document.createElement("span");
   const spanForLong = document.createElement("span");
   spanForLat.innerText = `latitude: ${parsedCoords.latitude}`;
   spanForLong.innerText = `longitude: ${parsedCoords.longitude}`;
-  geo_info.appendChild(spanForLat);
-  geo_info.appendChild(spanForLong);
+  wheather_info.appendChild(spanForLat);
+  wheather_info.appendChild(spanForLong);
 }
 
 function saveCoords(coordsObj) {
